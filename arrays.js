001494b3b524cc6816cb9bfcadee6eb76567c1e4
Mark Cruz <markchriscruz@yahoo.com>
2018-02-13 00:22:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,7 @@ function removeElementFromBeginningOfArray(array) {
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array) {
